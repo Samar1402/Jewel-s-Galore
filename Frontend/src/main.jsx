3c9f2d979c57106dace87264acd5844d6b43c5d3
Samar1402 <ksamarjeet320@gmail.com>
2025-12-04 11:57:29 +0530

    remove comments

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,13 @@
-// src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// Import Providers
 import { AuthProvider } from "./Context/AuthContext.jsx";
 import { CartProvider } from "./Components/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* CRITICAL: AuthProvider must wrap App */}
     <AuthProvider>
       <CartProvider>
         <App />
